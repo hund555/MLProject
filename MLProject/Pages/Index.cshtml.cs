c3ff002d5be6ee77f8ec1fd5b349ad81f8d8eb77
Hund555 <allanholmiversen@gmail.com>
2022-04-08 09:51:10 +0200

    frontend not needed ml works

--- MLProject/Pages/Index.cshtml.cs
+++ MLProject/Pages/Index.cshtml.cs
@@ -1,20 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static MLProject.MLModelAnimal;
 
 namespace MLProject.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IWebHostEnvironment _webHost;
+        public IndexModel(IWebHostEnvironment webHost)
         {
-            _logger = logger;
+            _webHost = webHost;
         }
+
+        [BindProperty]
+        public IFormFile Picture { get; set; }
+
+        public string ResultOutput { get; set; }
+        public string PicturePath { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task OnPostAsync()
+        {
+            string file = Path.Combine(_webHost.WebRootPath, "Images", Picture.FileName);
+            using (FileStream fileStream = new FileStream(file, FileMode.Create))
+            {
+                await Picture.CopyToAsync(fileStream);
+            }
+
+            //Load sample data
+            ModelInput userInput = new()
+            {
+                ImageSource = file,
+            };
+
+            //Load model and predict output
+            ModelOutput result = MLModelAnimal.Predict(userInput);
+
+            PicturePath = Picture.FileName;
+            //PicturePath = file;
+            if (result.Prediction == "Cow")
+            {
+                MLModelCow.ModelInput refineResult = new()
+                {
+                    ImageSource = userInput.ImageSource
+                };
+
+                MLModelCow.ModelOutput cowResult = MLModelCow.Predict(refineResult);
+                ResultOutput = cowResult.Prediction + ". Score: " + (cowResult.Score.OrderByDescending(x => x).First() * 100).ToString("n2") + "%";
+            }
+            else
+            {
+                ResultOutput = result.Prediction + ". Score: " + (result.Score.OrderByDescending(x => x).First() * 100).ToString("n2") + "%";
+            }
         }
     }
 }
